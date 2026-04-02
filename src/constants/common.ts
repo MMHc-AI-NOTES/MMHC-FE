3e21a1f34793f0029e0c39f5b33ff2636bfb34aa
bstali <bstali56@gmail.com>
2026-04-02 14:30:52 +0500

    Added custom model v3

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -424,9 +424,8 @@ export const AgentModelKeys = {
   NOVA_PREMIER: 'us.amazon.nova-premier-v1:0',
   CUSTOM_DEPLOYMENT_V1_15032026: 'arn:aws:bedrock:us-east-1:199990519622:custom-model-deployment/y5k4mxdxqxbx',
   CUSTOM_DEPLOYMENT_V2_28032026: 'arn:aws:bedrock:us-east-1:199990519622:custom-model-deployment/sugrzn0ke8p6',
+  CUSTOM_DEPLOYMENT_V3_02042026: 'arn:aws:bedrock:us-east-1:199990519622:custom-model-deployment/cxwsnnx1qsw8',
 } as const;
-
-export const agentModelKeys = {} as const;
 
 export const AgentModelDisplayNames: Record<keyof typeof AgentModelKeys, string> = {
   CLAUDE_3_HAIKU: 'Claude 3 Haiku',
@@ -439,6 +438,7 @@ export const AgentModelDisplayNames: Record<keyof typeof AgentModelKeys, string>
   NOVA_PREMIER: 'Nova Premier',
   CUSTOM_DEPLOYMENT_V1_15032026: 'Custom Fine-tuned Model V1 15032026',
   CUSTOM_DEPLOYMENT_V2_28032026: 'Custom Fine-tuned Model V2 28032026',
+  CUSTOM_DEPLOYMENT_V3_02042026: 'Custom Fine-tuned Model V3 02042026',
 };
 
 export const AgentTypes = {
